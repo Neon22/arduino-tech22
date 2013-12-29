329c763fc605b9f1b34b66ba0a206201f358f9ef
Mark Schafer <mschafer@wireframe.biz>
2013-12-29 15:40:11 +1300

    tabs and spelling

--- DigisparkTech22.cpp
+++ DigisparkTech22.cpp
@@ -5,85 +5,94 @@
 #include "DigisparkTech22.h"
 #include "Arduino.h"
 
-#define set(x) |= (1<<x) 
-#define clr(x) &=~(1<<x) 
-#define inv(x) ^=(1<<x)
+#define set(x) |=  (1<<x) 
+#define clr(x) &=~ (1<<x) 
+#define inv(x) ^=  (1<<x)
 
-#define CH3_CLEAR (pinlevelB &= ~(1 << CH3)) // 
-#define CH2_CLEAR (pinlevelB &= ~(1 << CH2)) // map CH2 to PB2
-#define CH1_CLEAR (pinlevelB &= ~(1 << CH1)) // 
-#define CH0_CLEAR (pinlevelB &= ~(1 << CH0)) // 
-#define PORTB_MASK  (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3)
+// map ports here
 #define CH3 PB3
-#define CH2 PB2
+#define CH2 PB2 // e.g. map CH2 to PB2
 #define CH1 PB1
 #define CH0 PB0
+
+#define CH3_CLEAR (pinlevelB &= ~(1 << CH3))
+#define CH2_CLEAR (pinlevelB &= ~(1 << CH2))
+#define CH1_CLEAR (pinlevelB &= ~(1 << CH1))
+#define CH0_CLEAR (pinlevelB &= ~(1 << CH0))
+#define PORTB_MASK  (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3)
 
 
 unsigned char DigisparkPWMcompare[4]; 
 volatile unsigned char DigisparkPWMcompbuff[4];
 
-void DigisparkTech22Begin() {                
+void DigisparkTech22Begin() {
 
-  pinMode(3, OUTPUT); 
-  pinMode(2, OUTPUT); 
-  pinMode(1, OUTPUT); 
-  pinMode(0, OUTPUT); 
-  //CLKPR = (1 << CLKPCE);        // enable clock prescaler update
-  //CLKPR = 0;                    // set clock to maximum (= crystal)
+	// set the pins to output
+	pinMode(3, OUTPUT); 
+	pinMode(2, OUTPUT); 
+	pinMode(1, OUTPUT); 
+	pinMode(0, OUTPUT); 
+	//CLKPR = (1 << CLKPCE);		// enable clock prescaler update
+	//CLKPR = 0;					// set clock to maximum (= crystal)
 
+	// set default PWM values
+	DigisparkPWMcompare[0] = 0x00;
+	DigisparkPWMcompare[1] = 0x00;
+	DigisparkPWMcompare[2] = 0x00;
+	DigisparkPWMcompare[3] = 0x00;
+	// and for the buffer
+	DigisparkPWMcompbuff[0] = 0x00;
+	DigisparkPWMcompbuff[1] = 0x00;
+	DigisparkPWMcompbuff[2] = 0x00;
+	DigisparkPWMcompbuff[3] = 0x00;
 
-  DigisparkPWMcompare[0] = 0x00;// set default PWM values
-  DigisparkPWMcompare[1] = 0x00;// set default PWM values
-  DigisparkPWMcompare[2] = 0x00;// set default PWM values
-  DigisparkPWMcompare[3] = 0x00;// set default PWM values
-  DigisparkPWMcompbuff[0] = 0x00;// set default PWM values
-  DigisparkPWMcompbuff[1] = 0x00;// set default PWM values
-  DigisparkPWMcompbuff[2] = 0x00;// set default PWM values
-  DigisparkPWMcompbuff[3] = 0x00;// set default PWM values
-  
+	// setup interrupts
+	TIFR = (1 << TOV0);			// clear interrupt flag
+	TIMSK = (1 << TOIE0);		// enable overflow interrupt
+	TCCR0B = (1 << CS00);		// start timer, no prescale
 
-  TIFR = (1 << TOV0);           // clear interrupt flag
-  TIMSK = (1 << TOIE0);         // enable overflow interrupt
-  TCCR0B = (1 << CS00);         // start timer, no prescale
-
-  sei(); 
+	// turn on interrupts
+	sei(); 
 }
 
 void DigisparkTech22(int pin,int value){
+	// set a pin to a value
 	DigisparkPWMcompbuff[pin] = value;
 }
 
 int DigisparkTech22Read(int pin) {
+	// return value currently set at
 	return DigisparkPWMcompbuff[pin];
 }
 
 void DigisparkTech22Delay(int ms) {
-  while (ms) {
-    _delay_ms(1);
-    ms--;
-  }
+	// call system 1ms delay until done
+	while (ms) {
+		_delay_ms(1);
+		ms--;
+	}
 }
 
+// Interrupt service routine.
+// runs when sei() turns it on
 ISR (TIM0_OVF_vect) {
-  static unsigned char pinlevelB=PORTB_MASK;
-  static unsigned char softcount=0xFF;
+	static unsigned char pinlevelB=PORTB_MASK;
+	static unsigned char softcount=0xFF; // 8bit pwm
 
-  PORTB = pinlevelB;            // update outputs
-  
-  if(++softcount == 0){         // increment modulo 256 counter and update
-                                // the compare values only when counter = 0.
-    DigisparkPWMcompare[0] = DigisparkPWMcompbuff[0]; // verbose code for speed
-    DigisparkPWMcompare[1] = DigisparkPWMcompbuff[1]; // verbose code for speed
-    DigisparkPWMcompare[2] = DigisparkPWMcompbuff[2]; // verbose code for speed
-	DigisparkPWMcompare[3] = DigisparkPWMcompbuff[3]; // verbose code for speed
+	PORTB = pinlevelB;			// update outputs using mask
 
-    pinlevelB = PORTB_MASK;     // set all port pins high
-  }
-  // clear port pin on compare match (executed on next interrupt)
-  if(DigisparkPWMcompare[0] == softcount) CH0_CLEAR;
-  if(DigisparkPWMcompare[1] == softcount) CH1_CLEAR;
-  if(DigisparkPWMcompare[2] == softcount) CH2_CLEAR;
-  if(DigisparkPWMcompare[3] == softcount) CH3_CLEAR;
+	if(++softcount == 0){			// increment modulo 256 counter and update
+									// the compare values only when counter = 0.
+		DigisparkPWMcompare[0] = DigisparkPWMcompbuff[0]; // verbose code for speed
+		DigisparkPWMcompare[1] = DigisparkPWMcompbuff[1]; // verbose code for speed
+		DigisparkPWMcompare[2] = DigisparkPWMcompbuff[2]; // verbose code for speed
+		DigisparkPWMcompare[3] = DigisparkPWMcompbuff[3]; // verbose code for speed
+		pinlevelB = PORTB_MASK;		// set all port pins high
+	}
+	// clear port pin on compare match (executed on next interrupt)
+	if(DigisparkPWMcompare[0] == softcount) CH0_CLEAR;
+	if(DigisparkPWMcompare[1] == softcount) CH1_CLEAR;
+	if(DigisparkPWMcompare[2] == softcount) CH2_CLEAR;
+	if(DigisparkPWMcompare[3] == softcount) CH3_CLEAR;
 }
 
--- examples/DigisparkTech22/DigisparkTech22.ino
+++ examples/DigisparkTech22/DigisparkTech22.ino
@@ -3,69 +3,79 @@
  Digispark Tech22
  
  This example shows how to use soft PWM to fade4 channels.
- Note: This is only necessary for PB2 (pin 2) - CH3, as CH0 (pin 0) and CH1 (pin 1) 
- as well as pin 4 support the standard Arduino analogWrite() function.
+ Note: This implements software PWM even though its not required for pins 0,1 (CH 0,1) 
+ - these two pins have hardware PWM support courtesy of the standard Arduino analogWrite() function.
+ But we do all 4 here in software PWM.
  
  This example code is in the public domain.
  */
+ 
 byte CH0 = 0;
-byte CH2 = 2;
 byte CH1 = 1;
+byte CH2 = 2;
 byte CH3 = 3;
-byte CHANNELS[] = {
-  CH0,CH1,CH2,CH3};
+byte CHANNELS[] = {CH0,CH1,CH2,CH3}; // use this for easy access to all 4 channels
 
 // the setup routine runs once when you press reset:
 void setup()  { 
-  DigisparkTech22Begin();
+	// setup the software interrupt based PWM routines
+	DigisparkTech22Begin();
 } 
 
-
+// runs after setup and loops forever
 void loop ()  {
-  //direction: up = true, down = false
-  boolean dir = true;
-  int i = 0;
-
-  while(1)  {
-    //fade(CHANNELS[i%4], dir);
-    fadeto(CHANNELS[0], 125);
-    i++;
-    dir = !dir;
-  }
+	//direction: up = true, down = false
+	boolean dir = true;
+	int i = 0;
+	while(1)  {
+		//fade(CHANNELS[i%4], dir);
+		// simple example fades channel zero up
+		fadeto(CHANNELS[0], 125);
+		i++;
+		dir = !dir;
+	}
 }
 
+// General functions that control a channel and do not need to be adjusted
+// Just call them in your loop code.
+
+
 void fadeto(byte Led, int val) {
-  int i;
-  int current;
-  current = DigisparkTech22Read(Led);
-  if (current > val) {
-    //if fading up
-    for (i = current; i < val; i++) {
-      DigisparkTech22(Led, i);
-      DigisparkTech22Delay(125);
-    }
-  }
-  else {
-    for (i = current; i >= val; i--) {
-      DigisparkTech22(Led, i);
-      DigisparkTech22Delay(25);
-    }
-  }
+	// read the current value and fade from wherever we are now to the new value.
+	int i;
+	int current;
+	current = DigisparkTech22Read(Led);
+	if (current > val) {
+		//if fading up
+		for (i = current; i < val; i++) {
+			DigisparkTech22(Led, i);
+			DigisparkTech22Delay(125);
+		}
+	}
+	else {
+		for (i = current; i >= val; i--) {
+			DigisparkTech22(Led, i);
+			DigisparkTech22Delay(25);
+		}
+	}
 }
 
 void fade(byte Led, boolean dir) {
-  int i;
-  //if fading up
-  if (dir) {
-    for (i = 0; i < 256; i++) {
-      DigisparkTech22(Led, i);
-      DigisparkTech22Delay(125);//1);
-    }
-  }
-  else {
-    for (i = 255; i >= 0; i--) {
-      DigisparkTech22(Led, i);
-      DigisparkTech22Delay(25);//1);
-    }
-  }
+	// fade up to maximum or,
+	// fade down to minimum
+	int i;
+	
+	if (dir) {
+		// fading up
+		for (i = 0; i < 256; i++) {
+			DigisparkTech22(Led, i);
+			DigisparkTech22Delay(125);//1);
+		}
+	}
+	else { // fading down
+		for (i = 255; i >= 0; i--) {
+			DigisparkTech22(Led, i);
+			DigisparkTech22Delay(25);//1);
+		}
+	}
 }
